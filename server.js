61d6b69e5c789017b4347cd354e5b96561ac786d
cathytanya <cathycolette.tanya@gmail.com>
2021-10-24 15:06:17 -0400

    set express to app and port listener for heroku

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 // list of libraries needed for the appliation
+const { application } = require("express");
 const express = require("express");
 const fs = requires("fs");
 const path = express("path")
+// set express method to app
+const app = express()
+// make a port listener for heroku
+const PORT = process.env.PORT || 3001
