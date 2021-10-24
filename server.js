a78130cdcaf09e9fc4148a6975d86019cfd6c175
cathytanya <cathycolette.tanya@gmail.com>
2021-10-24 15:27:48 -0400

    made 3 route gets

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // list of libraries needed for the appliation
-const { application } = require("express");
 const express = require("express");
-const fs = requires("fs");
-const path = express("path")
+const fs = require("fs");
+const path = require("path")
 // set express method to app
 const app = express()
 // make a port listener for heroku
 const PORT = process.env.PORT || 3001
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+// route to index.html
+app.get("/notes",(req,res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+})
+// route to noteList.html
+app.get("/notes",(req,res) => {
+    res.sendFile(path.join(__dirname,"/public/notes.html"))
+})
+// route to db.json
+app.get("/notes",(req,res) => {
+    res.sendFile(path.join(__dirname,"/db/db.json"))
+})
